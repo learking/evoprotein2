ae262264ac7199b94acfb2db9f3ace0c8bbe94ff
Kuangyu Wang <wangw1023@gmail.com>
2012-10-01 15:25:05 -0400

    seqStructCompatibility created

---
+++ src/evoprotein/proteinstructure/SeqStructCompatibility.java
@@ -0,0 +1,20 @@
+package evoprotein.proteinstructure;
+
+import beast.core.Plugin;
+import evoprotein.evolution.datatype.MutableSequence;
+
+public class SeqStructCompatibility extends Plugin {
+	
+	// required input: structureEnv
+	
+	// required input: inputStructure
+	
+	
+	
+	public double calcSeqStructProb (MutableSequence seq) {	
+		
+		
+		return 1.0;
+	}
+	
+}
