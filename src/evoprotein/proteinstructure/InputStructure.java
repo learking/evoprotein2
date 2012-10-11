679a98e38b29d988fcbc48414c618defb2d8edec
Kuangyu Wang <wangw1023@gmail.com>
2012-10-11 14:26:55 -0400

    mock up half done

--- src/evoprotein/proteinstructure/InputStructure.java
+++ src/evoprotein/proteinstructure/InputStructure.java
@@ -3,6 +3,7 @@ package evoprotein.proteinstructure;
 import beast.core.Input;
 import beast.core.Plugin;
 import beast.core.Input.Validate;
+import beast.util.Randomizer;
 
 public class InputStructure extends Plugin {
 	
@@ -20,7 +21,7 @@ public class InputStructure extends Plugin {
 	public void initAndValidate(){
 		// need to initiate AA seq length
 		AAseqLength = 100;
-		
+
 		parseInputStructure();
 	}
 	
@@ -35,32 +36,47 @@ public class InputStructure extends Plugin {
 	}
 	
 	// first order
-	public int [] parseFirstOrderTerms(){
+	public void parseFirstOrderTerms(){
 		// for now, mock up a list that logs which solvent category this position belongs to
 		// length of the list should be equal to AAseqLength
-		int [] firstOrder = new int [AAseqLength];
-		double [] pdf = new double [solventAccessibility.get().numOfCategories()];
-		
-		
-		return firstOrder;
+		firstOrderTerms = mockUpFirstOrderTerms();
 	}
 	
 	// second order
 	public void parseInteractionTerm2EnvMap(){
 		// sparse matrix might be a better choice, but for now, I will use matrix
 		// for now, mock up a list that logs which environment this pair of AA under consideration belongs to
-		
-		
+		interactionTerm2EnvMap = mockUpInteractionTerm2EvnMap();
 	}
 	
 	// for test only
-	public void mockUpFirstOrderTerms(){
+	public int[] mockUpFirstOrderTerms(){
 		// for categories available, randomly assign category to each position
+		int [] firstOrder = new int [AAseqLength];
+		double [] pdf = new double [solventAccessibility.get().numOfCategories()];
 		
+		double tempSum = 0;
+		for (int i = 0; i < pdf.length; i++) {
+			tempSum += 1.0 / (double) pdf.length;
+			pdf[i] = tempSum;
+		}
+		
+		for (int i = 0; i < firstOrder.length; i++) {
+			firstOrder[i] = Randomizer.randomChoice(pdf);
+		}
+		
+		return firstOrder;
 	}
 	
-	public void mockUpInteractionTerm2EvnMap(){
+	public int[][] mockUpInteractionTerm2EvnMap(){
+		int[][] interactionTerm2EvnMap = new int[AAseqLength][AAseqLength];
+
+		// assign about 3N of all the entries in the matrix to sum up to "1"
+		int nonZeroEntryNumber = AAseqLength * 3;
 		
+		
+		
+		return interactionTerm2EvnMap;
 	}
 	
 }
