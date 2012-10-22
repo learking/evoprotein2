2f23a6e5492fed0254d2c998034b696369e34ed5
Kuangyu Wang <wangw1023@gmail.com>
2012-10-22 16:11:31 -0400

    protein moeel started imple

--- src/beast/evolution/substitutionmodel/ProteinCodingDNASubstModel.java
+++ src/beast/evolution/substitutionmodel/ProteinCodingDNASubstModel.java
@@ -1,61 +1,75 @@
 package beast.evolution.substitutionmodel;
 
+import evoprotein.evolution.datatype.MutableSequence;
+import evoprotein.proteinstructure.InputStructure;
 import beast.core.CalculationNode;
+import beast.core.Input;
+import beast.core.Input.Validate;
+import beast.core.parameter.RealParameter;
 import beast.evolution.datatype.DataType;
 import beast.evolution.tree.Node;
 
-public class ProteinCodingDNASubstModel extends CalculationNode implements SubstitutionModel {
+public class ProteinCodingDNASubstModel extends CalculationNode {
 
 	// determine inputs
 	
+	//
+    //public Input<RealParameter> kappa = new Input<RealParameter>("kappa", "kappa parameter in our model", Validate.REQUIRED);
+	
+	// for use in neutral model
+	public Input<Frequencies> frequenciesInput =
+            new Input<Frequencies>("frequencies", "our substitution model equilibrium state frequencies", Validate.REQUIRED);
+	// for use in struct based model
+	public Input<InputStructure> m_inputStructure = new Input<InputStructure>("inputStructure", "input Structure pre-calculated", Validate.REQUIRED);
+
 	// define variables here
-	
+    Frequencies frequencies;
+    double freqA;
+    double freqC;
+    double freqG;
+    double freqT;
+    double Y;
 	// initAndValidate
-	
-	
-	
-	@Override
-	public void getTransitionProbabilities(Node node, double fStartTime,
-			double fEndTime, double fRate, double[] matrix) {
-		// TODO Auto-generated method stub
+    @Override
+    public void initAndValidate(){
+    	frequencies = frequenciesInput.get();
+    }
+    
+	public double getSubstitutionRate(MutableSequence seqI, MutableSequence seqJ) throws Exception{
+		double substitutionRate = 0;
+
+		// find where these two sequences differ (both location and value)
+		int differPosition = getDifferPosition(seqI, seqJ);
+		double tau = getStructBaseAndNeutralRatio(seqI, seqJ, differPosition);
 		
+		return substitutionRate;
 	}
-
-	// most important implementation
-	@Override
-	public double[] getRateMatrix(Node node) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public double[] getFrequencies() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getStateCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public EigenDecomposition getEigenDecomposition(Node node) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean canReturnComplexDiagonalization() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean canHandleDataType(DataType dataType) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
+    
+    int getDifferPosition(MutableSequence seqI, MutableSequence seqJ) throws Exception {
+		int[] seq_i = seqI.getSequence();
+		int[] seq_j = seqJ.getSequence();
+    	int differPosition = -1;
+    	int numOfDifferences = 0;
+    	for (int nucleoPosition = 0; nucleoPosition < seq_i.length; nucleoPosition++) {
+    		if(seq_i[nucleoPosition] != seq_j[nucleoPosition]){
+    			numOfDifferences++;
+    			differPosition = nucleoPosition;
+    		}
+    	}
+    	if(numOfDifferences == 1){
+    		return differPosition;
+    	}
+    	else{
+    		throw new Exception("");
+    	}
+    }
+    
+    double getStructBaseAndNeutralRatio(MutableSequence seqI, MutableSequence seqJ, int differPosition){
+    	double tau;
+    	double neutralSeqProbRatio = frequencies.getFreqs()[seqJ.getSequence()[differPosition]] / frequencies.getFreqs()[seqJ.getSequence()[differPosition]];
+    	double structBasedSeqProbRatio;
+    	tau = neutralSeqProbRatio;
+    	return tau;
+    }
+    
 }
---
+++ src/test/evoprotein/proteinstructure/ProteinCodingDNASubstModelTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package test.evoprotein.proteinstructure;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author kuangyu
+ *
+ */
+public class ProteinCodingDNASubstModelTest extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
