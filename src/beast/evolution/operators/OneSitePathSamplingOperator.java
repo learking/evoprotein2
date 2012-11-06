3cd482ef4c1292f3d8b84f7cf2b535924ed86f28
Kuangyu Wang <wangw1023@gmail.com>
2012-11-05 20:57:21 -0500

    new operator

--- src/beast/evolution/operators/OneSitePathSamplingOperator.java
+++ src/beast/evolution/operators/OneSitePathSamplingOperator.java
@@ -4,7 +4,12 @@
 package beast.evolution.operators;
 
 import beast.core.Description;
+import beast.core.Input;
 import beast.core.Operator;
+import beast.core.Input.Validate;
+import beast.evolution.sitemodel.SiteModel;
+import beast.evolution.substitutionmodel.SubstitutionModel;
+import beast.evolution.tree.PathTree;
 
 /**
  * @author kwang2
@@ -13,13 +18,38 @@ import beast.core.Operator;
 @Description("One site version of pathSamplingOperator, used in every MCMC round except initialization stage")
 public class OneSitePathSamplingOperator extends Operator {
 
-	/* (non-Javadoc)
-	 * @see beast.core.Operator#proposal()
-	 */
+	public Input<PathTree> m_pathTree = new Input<PathTree>("pathtree", "pathtree on which this operation is performed", Validate.REQUIRED);
+    public Input<SiteModel.Base> m_pSiteModel = new Input<SiteModel.Base>("siteModel", "site model for leafs in the beast.tree", Validate.REQUIRED);
+	
+    SubstitutionModel.Base m_substitutionModel;
+    double fHastingsRatio, newPathLogDensity;
+	
+    // lambda parameter for nextExponential()
+    final double lambda = 1.0;
+    
+    @Override
+    public void initAndValidate() throws Exception {
+    	m_substitutionModel = m_pSiteModel.get().m_pSubstModel.get();
+    }
+    
+    /*
+    @Override
+    public void accept(){
+    	super.accept();
+    	// when accept, update the value of oldPathLogDensity
+    	oldPathLogDensity = newPathLogDensity;
+    	
+    	// for debugging
+    	//estimateParameters();
+    }
+    */
+    
 	@Override
 	public double proposal() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	
+	
 }
---
+++ src/test/beast/evolution/operators/OneSitePathSamplingOperatorTest.java
@@ -0,0 +1,58 @@
+package test.beast.evolution.operators;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import beast.core.parameter.RealParameter;
+import beast.evolution.alignment.Alignment;
+import beast.evolution.operators.OneSitePathSamplingOperator;
+import beast.evolution.sitemodel.SiteModel;
+import beast.evolution.substitutionmodel.Frequencies;
+import beast.evolution.substitutionmodel.InstantHKY;
+import beast.evolution.tree.PathTree;
+import beast.evolution.tree.Tree;
+
+import junit.framework.TestCase;
+
+public class OneSitePathSamplingOperatorTest extends TestCase {
+
+	Alignment data;
+	Tree tree;
+	PathTree pathTree;
+	InstantHKY instantHKY;
+	RealParameter kappa;
+	Frequencies frequences;
+	SiteModel siteModel;
+	OneSitePathSamplingOperator oneSiteOperator;
+	
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+        data = getDummyAlignment();
+        //data = getAlignment();
+		
+        tree = getTree(data);
+		
+        pathTree = new PathTree();
+		pathTree.initByName("initial", tree, "alignment", data);
+        
+		kappa = getKappa("1.5");
+        
+        frequences = new Frequencies();
+        frequences.initByName("data", data);
+        
+        instantHKY = new InstantHKY();
+        instantHKY.initByName("kappa", kappa, "frequencies", frequences);
+        
+        siteModel = new SiteModel();
+        siteModel.initByName("gammaCategoryCount", 1, "substModel", instantHKY);
+        
+	}
+
+	@Test
+	public void testOneSiteOperator() {
+		fail("Not yet implemented");
+	}
+
+}
